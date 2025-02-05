f4abd56fb712b3be41a6aeadce8a48a4658e4612
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2025-02-04 22:50:08 -0300

    feat: :sparkles: criação dos estados visuais nos componentes

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,16 +1,22 @@
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
 
 export function UploadWidgetUploadList() {
+  const isUploadListEmpty = true;
+
   return (
     <div className="px-3 flex flex-col gap-3">
       <span className="text-xs font-medium">
         Uploaded files <span className="text-zinc-400">(3)</span>
       </span>
-      <div className="flex flex-col gap-2">
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-      </div>
+      {isUploadListEmpty ? (
+        <span className="text-xs text-zinc-400">No uploads added</span>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+        </div>
+      )}
     </div>
   );
 }
